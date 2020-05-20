904fc538a0621506dd4d43541dfc04fc7a08ce26
Nurlybek Karshiga <nurlybek.karshyga@gmail.com>
2020-05-20 00:04:56 -0400

    adding authentication form to the sign-in page and adding new form-input and custom-button generic components

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss';
+import SignIn from '../../components/signin/signin.component';
+
+const SignInAndSignUpPage = () => (
+  <div className='sign-in-and-sign-out'>
+    <SignIn />
+  </div>
+);
+
+export default SignInAndSignUpPage;
